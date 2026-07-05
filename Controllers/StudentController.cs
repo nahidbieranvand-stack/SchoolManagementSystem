691e3c35b388faa1c4f4319defb8131c60f1f90f
nahidbieranvand <nahidbieranvand@gmail.com>
2026-07-05 15:42:17 +0330

    "Complete student edit feature"

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -7,7 +7,7 @@ using SchoolManagementSystem.Models;
 
 namespace SchoolManagementSystem.Controllers
 {
-   
+    
     public class StudentController : Controller
     {
         private readonly SchoolDbContext _context;
@@ -15,6 +15,8 @@ namespace SchoolManagementSystem.Controllers
         {
            _context = context;
         }
+
+      
         [HttpGet]
         public IActionResult Index()
         {
@@ -22,8 +24,75 @@ namespace SchoolManagementSystem.Controllers
             var students=_context.Students.ToList();
             return View(students);
         }
+        [HttpGet]
+        public IActionResult creat()
+        {
+            return View();
+            
+        }
+        private bool NationalCodeExists(string nationalCode)
+        {
+            return _context.Students
+                           .Any(s => s.NationalCode == nationalCode);
+        }
+
+        [HttpPost]
+        public IActionResult Creat(Student student)
+        {
+            //Console.WriteLine(student.FirstName);برای اینکه ببینم مقدار میگیرند یانه بریک پوینت نیذارین و اجره
+            // Console.WriteLine(student.LastName);
+            if (!ModelState.IsValid)
+                return View(student);
+            
+            if (NationalCodeExists(student.NationalCode))
+            {
+                ModelState.AddModelError("NationalCode",
+                    "این کد ملی قبلاً ثبت شده است.");
+
+                return View(student);
+            }
+            student.RegisterDate = DateTime.Now;
+            student.IsActive = true;
+            _context.Students.Add(student);
+            //student.RegisterDate = DateTime.Now;
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+           var student=_context.Students.Find(id);
+            if (student == null){
+                return NotFound();
+            }  return View(student);     
+        
+
     }
-    
-    
-    
-}
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            if (!ModelState.IsValid)
+                return View(student);
+            _context.Students.Update(student);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+         
+
+    }
+
+
+//if (ModelState.IsValid)
+           // {
+              //  _context.Students.Add(student);
+//_context.SaveChanges();
+               // return RedirectToAction(nameof(Index));
+//}
+          //  return View(student);
+
+        
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 namespace SchoolManagementSystem.Models
 {
     public enum Gender
@@ -9,29 +10,43 @@ namespace SchoolManagementSystem.Models
     public class Student
     {
         public int Id { set; get; }
-        [Required] [StringLength (50)]
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "وارد کردن نام الزامی است.")]
+        [StringLength(50)]
         public string FirstName { get; set; }
-        [Required] [StringLength (50)]
+        [Display(Name = " نام خانوادگی")]
+        [Required(ErrorMessage = "وارد کردن نام خانوادگی الزامی است.")]
+        [StringLength(50)]
         public string LastName { get; set; }
-        [Required]
-        [StringLength (10,MinimumLength =10)]
-        [RegularExpression(@"^09\d{9}$",
-         ErrorMessage = "شماره ملی معتبر نیست")]
+        [Display(Name = " کدملی")]
+        [Required(ErrorMessage = "کد ملی الزامی است.")]
+        [StringLength(10,
+          MinimumLength = 10,
+          ErrorMessage = "کد ملی باید 10 رقم باشد.")]
         public string NationalCode{ set; get; }
+        [Display(Name = " تاریخ تولد")]
         [Required]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { set; get; }
-        [Required]
+        [Display(Name = " اسم پدر")]
+        [Required(ErrorMessage = "نام پدر الزامی است.")]
         public string FatherName{ set; get; }
-        [Required]
+        [Display(Name = "   شماره تلفن")]
+        [Required(ErrorMessage = "شماره تلفن الزامی است.")]
+        
         [StringLength (11,MinimumLength =11)]
         [RegularExpression(@"^09\d{9}$",
-         ErrorMessage = "شماره موبایل معتبر نیست")]
+         ErrorMessage = "شماره همراه معتبر نیست")]
         public string PhoneNumber { set; get; }
+        [Display(Name = " فعال بودن ")]
         public bool IsActive { set; get; }
+        [Display(Name = " پایه تحصیلی")]
         [Range(1,12)]
         public  int Grade {  set; get; }
+        
         public DateTime RegisterDate { get; set; }
-        [Required]
+        [Display(Name = "جنسیت")]
+        [Required(ErrorMessage = "جنسیت الزامی است.")]
         public Gender Gender { set; get; }
     }
 }
